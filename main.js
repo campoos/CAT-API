dc2f31186b2d0b11cef249eec4e3051b624c2ffb
campoos <joaosantos20071009@gmail.com>
2025-02-18 16:12:28 -0300

    site gato feito

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict"
+
+async function pesquisarFotos(){
+    const url = `https://api.thecatapi.com/v1/images/search?limit=99`
+
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.map(cat => cat.url)
+}
+
+function criarImagem(link){
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img')
+    novaImg.src = link
+
+
+    galeria.appendChild (novaImg)
+
+}
+
+async function preencherFotos(){
+    const fotos = await pesquisarFotos()
+    const galeria = document.getElementById('galeria')
+
+    galeria.replaceChildren('')
+    fotos.forEach(criarImagem)
+}
+
+preencherFotos()
